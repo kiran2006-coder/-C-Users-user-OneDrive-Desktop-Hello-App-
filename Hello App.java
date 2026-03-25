e4939a14dd8077a6bee19a13047c689dcadf194b
PULI VENKATA KIRAN REDDY <pulivenkatakiranreddy2006@gmail.com>
2026-03-25 08:29:39 +0530

    Add files via upload
    public class App {
        public static void main(String[] args) {
            if (args.length > 0) {
                String names = String.join(", ", args);
                System.out.println("Hello, " + names + "!");
            } else {
                System.out.println("Hello, World!");
            }
        }
    }

--- Hello App.java
+++ Hello App.java
@@ -1,6 +1,10 @@
-public class HelloApp {
+public class App {
     public static void main(String[] args) {
-        string = args[0];
-        System.out.println("Hello, " + name + "!");
+        if (args.length > 0) {
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
     }
 }
